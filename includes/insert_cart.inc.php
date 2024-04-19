4a967adff24cd605a43fcbf9328928178261984f
Jullian Quiambao <jullian@gmail.com>
2024-04-20 02:57:25 +0800

    pushed insert_cart.php, renamed adminpanel to index.php

--- includes/insert_cart.inc.php
+++ includes/insert_cart.inc.php
@@ -1,37 +1,47 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $productID = $_POST['productID'];
-    $quantity = $_POST['quantity'];
+    $quantity = isset($_POST['quantity']) ? (int)$_POST['quantity'] : 1;
     try {
         require_once("dbh.inc.php");
         require_once("config.inc.php");
-        // logic for checking quantity
-        $check_quantity = ("SELECT * FROM products WHERE productID = '$productID';");
-        $check_quantity = $pdo->prepare($check_quantity);
-        $check_quantity->execute();
-        $res = $check_quantity->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($res as $key) {
-            if ($key['productQuantity'] < $quantity) {
-                // logic for inserting into cart
-                $query = ("INSERT INTO cart_items (userID, productID, quantity) VALUES (:userID, :productID, :quantity);");
-                $statement = $pdo->prepare($query);
-                $statement->bindParam(":userID", $_SESSION['userID']);
-                $statement->bindParam(":productID", $productID);
-                $statement->bindParam(":quantity", $key['productQuantity']);
-                $statement->execute();
-            } else {
-                // logic for inserting into cart
-                $query = ("INSERT INTO cart_items (userID, productID, quantity) VALUES (:userID, :productID, :quantity);");
-                $statement = $pdo->prepare($query);
-                $statement->bindParam(":userID", $_SESSION['userID']);
-                $statement->bindParam(":productID", $productID);
-                $statement->bindParam(":quantity", $quantity);
-                $statement->execute();
-            } 
+        // checks to see quantity in the products table to see if it is less than the quantity in the cart
+        function checkQuantity($pdo, $productID, $quantity) {
+            $check_quantity = "SELECT * FROM products WHERE productID = ?";
+            $check_quantity_stmt = $pdo->prepare($check_quantity);
+            $check_quantity_stmt->execute([$productID]); 
 
+            $res = $check_quantity_stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($res as $key) {
+                if ($key['productQuantity'] < $quantity) {
+                    $quantity_to_add = $key['productQuantity'];
+                } else {
+                    $quantity_to_add = $quantity;
+                }
+                return $quantity_to_add;
+            }
         }
-        
+
+        $check_query = "SELECT * FROM cart_items WHERE productID = ? AND userID = ?;";
+        $stmt_check = $pdo->prepare($check_query);
+        $stmt_check->execute([$productID, $_SESSION['userID']]);
+        $existing_product = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if ($existing_product) {
+            $new_quantity = $existing_product['quantity'] + $quantity;
+            // change the quantity in the cart
+            $update_query = "UPDATE cart_items SET quantity = ? WHERE productID = ? AND userID = ?";
+            $stmt_update = $pdo->prepare($update_query);
+            $stmt_update->execute([$new_quantity, $productID, $_SESSION['userID']]);
+        } else {
+            $quantity_to_add = checkQuantity($pdo, $productID, $quantity);
+            // Prepare and execute the insert statement
+            $query = "INSERT INTO cart_items (userID, productID, quantity) VALUES (?, ?, ?)";
+            $statement = $pdo->prepare($query);
+            $statement->execute([$_SESSION['userID'], $productID, $quantity_to_add]);
+        }
+
         header("Location: ../products.php"); // Redirect back to the products page
     } catch (PDOException $e) {
         die("Error: " . $e->getMessage());
