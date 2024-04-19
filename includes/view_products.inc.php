d51c1ad41fb626ecc7dbda50b73cdcece86b33e3
Jullian Quiambao <jullian@gmail.com>
2024-04-20 02:59:43 +0800

    pushed view products

--- includes/view_products.inc.php
+++ includes/view_products.inc.php
@@ -17,32 +17,12 @@ try {
     // Fetch all rows from the result set
     $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     // Database connection and product retrieval logic
-
-    // JavaScript functions outside the loop
-    echo "
-        <script>
-            function increment(quantityID, maxQuantity) {
-                var quantity = document.getElementById(quantityID);
-                if (parseInt(quantity.value) < maxQuantity) {
-                    quantity.value = parseInt(quantity.value) + 1;
-                }
-            }
-
-            function decrement(quantityID) {
-                var quantity = document.getElementById(quantityID);
-                if (parseInt(quantity.value) > 1) {
-                    quantity.value = parseInt(quantity.value) - 1;
-                }
-            }
-        </script>
-    ";
-
     foreach ($products as $product) {
         // Construct the correct image path
         $db_quant = $product['productQuantity'];
         $db_prodID = $product['productID'];
         $db_prodPrice = $product['productPrice'];
-        $imagePath = "images/uploads/products/" . basename($product['imagePath']);
+        $imagePath = "images/" . basename($product['imagePath']);
         echo "
         <div class='card'>
             <div class='img'><img src='{$imagePath}' alt='{$product['productName']}'></div>
@@ -50,19 +30,35 @@ try {
             <div class='title'> {$product['productDesc']}</div>
             <div class='box'>
                 <div class='qty-btn'>
-                    <button onclick='decrement(\"quantity{$db_prodID}\")'>-</button>
-                    <button onclick='increment(\"quantity{$db_prodID}\", {$db_quant})'>+</button>
+                    <button id='decrement' onclick='decrement(quantity{$db_prodID})'>-</button>
+                    <button id='increment' onclick='increment(quantity{$db_prodID}, {$db_quant})'>+</button>
                 </div>
                 <form action='includes/insert_cart.inc.php' method='post' class='form'>
-                    <input type='number' class='quantity' id='quantity{$db_prodID}' name='quantity{$db_prodID}' value='1' min='1' max='$db_quant'>
-                    <div class='price'> ₱ {$db_prodPrice} </div>
-                    <input type='hidden' name='productID' value='{$db_prodID}'>          
-                    ";
-        checkIfLoggedIn();
-        echo "  </form>     
-            </div>    
-        </div>";
+                        <input type='number' class='quantity' id='quantity{$db_prodID}' name='quantity' value='1' min='1' max='$db_quant'>
+                        <div class='price'> ₱ {$db_prodPrice} </div>
+                        <input type='hidden' name='productID' value='{$db_prodID}'>          
+                        ";
+            checkIfLoggedIn();
+            echo "  </form>     
+                </div>    
+        </div> ";
     }
+    echo "
+            <script>
+                let quantity = document.getElementById('quantity{$db_prodID}');
+                function increment(quantity, maxQuantity) {
+                    if (parseInt(quantity.value) < maxQuantity) {
+                        quantity.value = parseInt(quantity.value) + 1;
+                    }
+                }
+
+                function decrement(quantity) {
+                    if (parseInt(quantity.value) > 1) {
+                        quantity.value = parseInt(quantity.value) - 1;
+                    }
+                }
+            </script>
+    ";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
